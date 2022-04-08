b550a07c0e4fb92a01cc91f45a638be8d2802acf
Masaya Tk <844196@gmail.com>
2022-04-08 16:34:26 +0000

    ✨ inject badge in followers list page

---
+++ src/features/decorate-artist/ArtistBadge.tsx
@@ -0,0 +1,20 @@
+import { VFC } from 'react';
+import { useAjaxUser } from './useAjaxUser';
+
+export const ArtistBadge: VFC<{ userId: string }> = ({ userId }) => {
+  const ajaxUser = useAjaxUser(userId);
+  if (!ajaxUser) {
+    return null;
+  }
+
+  const { body: { illusts, novels, novelSeries }} = ajaxUser;
+  const hasIllust = Array.isArray(illusts) === false;
+  const hasNovel = Array.isArray(novels) === false || novelSeries.length > 0;
+
+  return (
+    <>
+      {hasIllust && '🎨'}
+      {hasNovel && '🖊️'}
+    </>
+  );
+};
