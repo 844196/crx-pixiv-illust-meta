61625c42d8019fa03874a8709d469fa704640b21
Masaya Tk <844196@gmail.com>
2022-05-03 19:00:09 +0000

    ♻️ refactor mount

--- src/main.tsx
+++ src/main.tsx
@@ -4,29 +4,29 @@ import { App } from './App';
 import { IllustIdSchema } from './types/IllustId';
 import './vendors/setup-dayjs';
 
+const DATA_NAME = 'data-illust-meta';
+
+function mount(a: HTMLAnchorElement) {
+  a.setAttribute(DATA_NAME, 'processing');
+
+  try {
+    const illustId = IllustIdSchema.parse(a.href.split('/artworks/').pop());
+    const container = document.createElement('div');
+
+    a.parentElement?.after(container);
+    render(<App illustId={illustId} />, container);
+
+    a.setAttribute(DATA_NAME, 'processed');
+  } catch (err) {
+    a.setAttribute(DATA_NAME, 'mount-failure');
+    throw err;
+  }
+}
+
 function main() {
-  document.querySelectorAll<HTMLAnchorElement>('div[type="illust"] ~ div > a[href^="/artworks/"]:not([data-has-illust-meta])').forEach((a) => {
-    const parsedIllustId = IllustIdSchema.safeParse((a.getAttribute('href') ?? '').replace('/artworks/', ''));
-    if (!parsedIllustId.success) {
-      a.setAttribute('data-has-illust-meta', 'skip');
-      return;
-    }
-
-    a.setAttribute('data-has-illust-meta', 'processing');
-
-    try {
-      const container = document.createElement('div');
-      a.parentElement?.after(container);
-
-      render(<App illustId={parsedIllustId.data} />, container);
-
-      a.setAttribute('data-has-illust-meta', 'yes');
-    } catch (err) {
-      console.error(err);
-
-      a.setAttribute('data-has-illust-meta', 'cancel');
-    }
-  });
+  document
+    .querySelectorAll<HTMLAnchorElement>(`div[type="illust"] ~ div > a[href*="/artworks/"]:not([${DATA_NAME}])`)
+    .forEach(mount);
 }
 
 new MutationObserver(main).observe(document.body, { childList: true, subtree: true });
