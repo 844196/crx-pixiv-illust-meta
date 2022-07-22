c62160f716217ecf744f3c0c7054c67cf1f26799
Masaya Tk <844196@gmail.com>
2022-07-22 13:52:26 +0000

    :wrench: split tsconfig for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json'],
+    project: ['./tsconfig.eslint.json'],
   },
   plugins: ['@typescript-eslint', 'testing-library'],
   extends: [
