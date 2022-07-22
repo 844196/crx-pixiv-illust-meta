b77e94cc398d5434640a6166b02a183b7271ca4d
Masaya Tk <844196@gmail.com>
2022-07-22 12:45:31 +0000

    :wrench: remove unnecessary config

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,13 +2,4 @@
 module.exports = {
   semi: true,
   singleQuote: true,
-  overrides: [
-    // なぜかJSオブジェクト扱いされるため、パーサーをJSONに変更しておく
-    {
-      files: '.github/renovate.json5',
-      options: {
-        parser: 'json',
-      },
-    },
-  ],
 };
