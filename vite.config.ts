701b2eb111e0564cf188c1ad5f2ac1fb1c250098
Masaya Tk <844196@gmail.com>
2022-07-22 17:39:59 +0000

    :bulb: add comment of vitest global api

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { manifest } from './manifest.config';
 export default defineConfig({
   plugins: [react(), crx({ manifest })],
   test: {
-    globals: true,
+    globals: true, // see https://vitest.dev/config/#globals
     environment: 'jsdom',
     setupFiles: './vitest.setup.ts',
     globalSetup: './vitest.setup.global.ts',
